caf37b6c0dc82ceba2f8d9761a0794cbe03e70fa
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-07 13:58:37 +0900

    unit 08 test

---
+++ dojang_08.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 11:47:56 2021
+
+@author: user
+"""
+
+#python dojang uint 08
+
+korean, english, math, science=map(int,input().split())
+
+print(korean>=90 and english>80 and math>85 and science>=80)
