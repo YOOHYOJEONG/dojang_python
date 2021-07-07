547ada6e6cd5fc12832a14dad900bbdf2b163aab
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-07 13:59:55 +0900

    unit 09 test

---
+++ dojang_09.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 11:47:56 2021
+
+@author: user
+"""
+
+#python dojang uint 09
+
+s = """\'Python\' is a \"programming language\"
+that lets you work quickly
+and
+integrate systems more effectively."""
+
+print(s)
