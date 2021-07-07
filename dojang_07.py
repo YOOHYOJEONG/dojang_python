4e1660cf3f386abc47b53240710354a9efa63e96
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-07 13:58:04 +0900

    unit 07 test

---
+++ dojang_07.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 11:47:56 2021
+
+@author: user
+"""
+
+#python dojang uint 07
+
+year, month, day, hour, minute, second = input().split()
+
+print(year, month, day, sep='-', end = 'T')
+
+print(hour, minute, second, sep=':')
+
