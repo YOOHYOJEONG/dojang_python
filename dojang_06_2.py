f48683974a8c977184efa482b6cc61541bf5dbf0
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-07 12:19:32 +0900

    unit 06 6.8 test

---
+++ dojang_06_2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 11:47:56 2021
+
+@author: user
+"""
+
+#python dojang uint 06 6.8
+
+a,b,c,d = map(int, input().split())
+
+print(int((a+b+c+d)/4))
