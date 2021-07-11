171a9a6cebe9b2205eea215c01344f45e55141d2
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-11 21:47:46 +0900

    unit 13.7 test

---
+++ dojang_13.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 21:44:43 2021
+
+@author: user
+"""
+
+#python dojang uint 13.7
+
+a=int(input())
+b=input()
+if b=='Cash3000':
+    a-=3000
+if b=='Cash5000':
+    a-=5000
+print(a)
+
