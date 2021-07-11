ec3b6d5bfb7ad8a59afd40b95b2c38f48aa6b30a
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-11 21:47:12 +0900

    unit 12.5 test

---
+++ dojang_12.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 21:44:43 2021
+
+@author: user
+"""
+
+#python dojang uint 12.5
+
+x=input().split()
+y=map(float,input().split())
+lux=dict(zip(x,y))
+print(lux)
