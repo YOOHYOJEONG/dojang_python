b046b4b6269298f5ac0df518e119d1704c0dc62a
YOOHYOJEONG <ryuhyojung@naver.com>
2021-07-16 22:13:44 +0900

    unit 16.6 test

---
+++ dojang_16.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 21:44:43 2021
+
+@author: user
+"""
+
+#python dojang uint 16.5
+
+x=int(input())
+for i in range(1,10):
+    print(x,'*',i,'=',x*i)
